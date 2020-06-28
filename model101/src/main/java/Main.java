a2f3c91a5186affcc4c185cb37c08e861c71cf27
black <black@pop-os.localdomain>
2020-06-28 18:17:53 -0400

    functional

--- model101/src/main/java/Main.java
+++ model101/src/main/java/Main.java
@@ -1,14 +1,19 @@
 import events.SocialEvent;
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 
 import javax.security.auth.login.LoginException;
 
 public class Main {
-    public static void main(String[]args) throws LoginException, InterruptedException {
-        final String token = "NzI2ODMwMDIzMjgxMDgyNTIw.XvkOMw.UBn3fFUA7-LM3-rVoknIMIhsmPE";
-        JDA jda = new JDABuilder(token).addEventListeners(new SocialEvent()).build();
-
-        jda.awaitReady();
+    public static void main(String[] args) throws LoginException
+    {
+        final String token = "";
+        new JDABuilder(token)
+                .addEventListeners(new SocialEvent())
+                .setActivity(Activity.playing("Searching for John Conner"))
+                .build();
     }
+
+
+
 }
--- model101/src/main/java/events/SocialEvent.java
+++ model101/src/main/java/events/SocialEvent.java
@@ -1,24 +1,19 @@
 package events;
-
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class SocialEvent extends ListenerAdapter {
 
 
-    public void onGuildMessageRecieved(GuildMessageReceivedEvent event){
-        //if (event.getAuthor().isBot()) return;
-        // We don't want to respond to other bot accounts, including ourself
-        Message message = event.getMessage();
-        String content = message.getContentRaw();
-        // getContentRaw() is an atomic getter
-        // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
-        if (content.equals("!ping"))
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        Message msg = event.getMessage();
+        if (msg.getContentRaw().equals("!ping"))
         {
             MessageChannel channel = event.getChannel();
-            channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
+            channel.sendMessage("Pong!").queue();
         }
     }
 }
