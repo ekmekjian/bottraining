384133e53e2b263c1f753965cd3747f2f2deb208
Ara Ekmekjian <ekmekjian.ara@protonmail.com>
2020-07-24 23:57:32 -0400

    messing with !search

--- model101/src/main/java/Main.java
+++ model101/src/main/java/Main.java
@@ -7,7 +7,7 @@ import javax.security.auth.login.LoginException;
 public class Main {
     public static void main(String[] args) throws LoginException
     {
-        final String token = "NzI2ODMwMDIzMjgxMDgyNTIw.Xvi_NQ.H_cF6AzuukTZezM-Mcbi6r6vAJw";
+        final String token = "NzI2ODMwMDIzMjgxMDgyNTIw.Xvi_NQ.psV_XNDjY725VPqgNKm5v7KlUoE";
         new JDABuilder(token)
                 .addEventListeners(new RedditEvent())
                 .setActivity(Activity.playing("Searching for John Conner"))
--- model101/src/main/java/events/RedditEvent.java
+++ model101/src/main/java/events/RedditEvent.java
@@ -4,19 +4,26 @@
 textChannel.sendMessage("MESSAGE").queue();
 */
 
-
 package events;
+
 import com.github.jreddit.entity.Submission;
 import com.github.jreddit.entity.User;
 import com.github.jreddit.retrieval.Submissions;
 import com.github.jreddit.retrieval.params.SubmissionSort;
 import com.github.jreddit.utils.restclient.HttpRestClient;
 import com.github.jreddit.utils.restclient.RestClient;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.io.IOException;
 import java.util.List;
 
 public class RedditEvent extends ListenerAdapter {
@@ -65,12 +72,30 @@ public class RedditEvent extends ListenerAdapter {
         }
         if(msg.getContentRaw().equals("!search"))
         {
+            MessageChannel channel = event.getChannel();
+            try {botSearch(channel);} catch (IOException e) { e.printStackTrace();}
 
         }
         if(msg.getContentRaw().equals("!8ball")){
             MessageChannel channel = event.getChannel();
             channel.sendMessage("Asking the 🎱 your question..");
         }
+    }
+    void botSearch(MessageChannel ch) throws IOException
+    {
+         /**
+             * Documents d = Jsoup.connect("url").get();
+             * v this is loading the container element that holds the acutal data you're looking for
+             * Elements ele =d.select("thetag#id")
+             * for(Element element : ele.select("thetag.class"))
+             */
+            Document d = Jsoup.connect("https://duckduckgo.com/?t=ffab&q=witchcraft&atb=v229-5rk&ia=web").get();
+            Elements ele = d.select("div#links");
+            for(Element element : ele.select("div.result"))
+            {
+                String title= element.select("dev.result__title a").text();
+                System.out.println(title);
+            }
     }
     List<Submission> pullMemeFeed(com.github.jreddit.entity.User user, RestClient restClient){
         
